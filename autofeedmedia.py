ceefc82b16cf6c38a45bb2528a43f29c0af3c32d
Mythesis002 <131542998+Mythesis002@users.noreply.github.com>
2025-03-05 11:18:11 +0530

    Update autofeedmedia.py

--- autofeedmedia.py
+++ autofeedmedia.py
@@ -188,39 +188,46 @@ def post_reel():
     # Main Image Overlay (Product/Feature Image)
       {
       'overlay': public_id,
-      'width': 1080,
-      'height': 1920,
+      'width': 400,
+      'height': 400,
       'crop': "pad",
-      'background': "auto:predominant_gradient:2:diagonal_desc"
+      'y': 130,
+      'background': "#000000", 'gravity': "north"
       },
+      {'background': "#000000", 'gravity': "north", 'height': 1920, 'width': 1080, 'crop': "pad"},
+      {'effect': "gradient_fade:symmetric_pad", 'x': "0.5"},
       {'effect': 'gen_restore'},
-      {'flags': "layer_apply", 'width': 1920,'crop': "pad"},
-      {"overlay": f"audio:{music_id}", "start_offset": "40", "duration": "15"},
+      {'effect': "fade:2000"},
+      {
+      'flags': "layer_apply",
+      'width': 1080,
+      'crop': "pad",
+      'gravity': "center",
+      'y': -130  # Moves image 100 pixels up
+      },
+      {"overlay": f"audio:{music_public_id}", "start_offset": "40", "duration": "15"},
+      {'effect':"volume:1000"},
       {'flags': "layer_apply"},
       {'width': 500, 'crop': "scale"},
+
         # Corrected text overlay parameters
       {
       'overlay': {
-      'font_family': "arial",
-      'font_size': 25,
-      'font_weight': "bold",
+      'font_family': "georgia",
+      'font_size': 30,
       'gravity': "center",
-      'text': summary
+      'y': -30,
+      'text_align': "center",
+      'text': headline
       },
-       'color': "white",
-       'background': "black",
-       'width': 400,
-       'crop': "fit",
-       'gravity': "center",  # Align text to the center
-       'border': "20px_solid_black"  # Padding effect using a border
-       },
-       {'flags': "layer_apply", 'gravity': "north", 'y': 500},
-       {'overlay': {'font_family': "arial", 'font_size': 20, 'font_weight': "bold", 'text': "Thetrendsfeed"}, 'color': "black", 'background': "skyblue", 'radius': 20, 'x': 20, 'y': 20, 'width': 400, 'crop': "fit"},
-       {'flags': "layer_apply", 'gravity': "north", 'y': 110},
-       {'overlay': {'font_family': "arial", 'font_size': 12, 'font_weight': "bold", 'text': "This page is totally handled by ai, which provides trending tech news faster than human!"}, 'color': "white", 'width': 300, 'crop': "fit"},
-       {'flags': "layer_apply", 'gravity': "north", 'y': 160},
-       {'overlay': {'font_family': "arial", 'font_size': 12, 'font_weight': "bold", 'text': "full details in caption"}, 'color': "white", 'width': 300, 'crop': "fit"},
-       {'flags': "layer_apply", 'gravity': "south", 'y': 150}
+      'color': "white",
+      'effect': "fade:2000",
+      'text_align': "center",
+      'width': 450,
+      'crop': "fit",
+      'gravity': "center",
+      'y': 100,# Align text to the center
+      }
     ])
 
     match = re.search(r'/webm"><source src="(.*\.mp4)"', str(video_url))
